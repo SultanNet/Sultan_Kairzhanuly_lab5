8a2eadbf988eaf22d78bfdcb9cc907ea88c8fc3a
SultanNet <190103319@stu.sdu.edu.kz>
2021-02-23 12:24:26 +0600

    changing routes

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,6 @@ Route::get('/test_id', function () {
 });
 
 Route::get('/post/{id}/{age}', function ($id,$age) {
-    return "Your id is: ".$id." Your age is: ".$age;
+    return "Your Id is: ".$id." Your age is: ".$age." GOOD DAY,SIR";
 });
 
